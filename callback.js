29b0b8478613b52482a6481eccf5298f90a6fa40
jonathan <josagal.ing@gmail.com>
2020-04-18 15:22:37 -0500

    llamado secuencial a la api para varios resultados - callback hell

--- callback.js
+++ callback.js
@@ -1,25 +1,23 @@
-// const API_URL = 'https://pokeapi.co/api/v2/'
-// const pokemon_url = 'pokemon/:id/'
-
-// const character_url = `${API_URL}${pokemon_url.replace(':id', 2)}`
-// const opts = { crossDomain: true } // le indica a Jquery que el request se realiza a otra página
-
-// const onPokeResponse = function(pokemon) {
-//     console.log(`Hola mi nombre es ${pokemon.name}`);
-// }
-
-
-// $.get(character_url, opts, onPokeResponse)
-
-
 const API_URL = 'https://pokeapi.co/api/v2/'
 const POKE_URL = 'pokemon/:id/'
-
 const opts = { crossDomain: true }
 
-const onPokeResponse = function(pokemon) {
-    console.log(`Hola mi nombre es ${pokemon.name}`);
+function obtenerPokemon(id, callback) {
+    const url = `${API_URL}${POKE_URL.replace(':id',id)}`
+    $.get(url, opts, function(pokemon) {
+        console.log(`Hola, mi nombre es ${pokemon.name}`);
+    })
+    if (callback) {
+        callback()
+    }
 }
 
-
-$.get(`${API_URL}${POKE_URL.replace(':id',1)}`, opts, onPokeResponse)
+obtenerPokemon(1, function() {
+    obtenerPokemon(2, function() {
+        obtenerPokemon(3, function() {
+            obtenerPokemon(4, function() {
+                obtenerPokemon(5)
+            })
+        })
+    })
+})
