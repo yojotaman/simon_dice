f97a4686e9e18e73afb7e2c1ce02b97378ec3b85
jonathan <josagal.ing@gmail.com>
2020-04-18 17:13:08 -0500

    Modificacion para llamado de promesas en paralelo

--- callback.js
+++ callback.js
@@ -33,26 +33,37 @@ function onError(id) {
     console.log(`Error obteniendo el id ${id}`);
 }
 
-obtenerPokemon(1)
-    .then(function(poke) {
-        console.log(`Hola soy el pokemon ${poke.name}`);
-        return obtenerPokemon(2)
-    })
-    .then(function(poke) {
-        console.log(`Hola soy el pokemon ${poke.name}`);
-        return obtenerPokemon(3)
-    })
-    .then(function(poke) {
-        console.log(`Hola soy el pokemon ${poke.name}`);
-        debugger
-        return obtenerPokemon(4)
-    })
-    .then(function(poke) {
-        console.log(`Hola soy el pokemon ${poke.name}`);
-        return obtenerPokemon(5)
-    })
-    .then(function(poke) {
-        console.log(`Hola soy el pokemon ${poke.name}`);
-        // return obtenerPokemon()
-    })
+let ids = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+let promesas = ids.map(id => obtenerPokemon(id))
+
+Promise.all(promesas)
+    .then(poke => console.log(poke))
     .catch(onError)
+
+
+
+
+// obtenerPokemon(1)
+//     .then(function(poke) {
+//         console.log(`Hola soy el pokemon ${poke.name}`);
+//         return obtenerPokemon(2)
+//     })
+//     .then(function(poke) {
+//         console.log(`Hola soy el pokemon ${poke.name}`);
+//         return obtenerPokemon(3)
+//     })
+//     .then(function(poke) {
+//         console.log(`Hola soy el pokemon ${poke.name}`);
+//         debugger
+//         return obtenerPokemon(4)
+//     })
+//     .then(function(poke) {
+//         console.log(`Hola soy el pokemon ${poke.name}`);
+//         return obtenerPokemon(5)
+//     })
+//     .then(function(poke) {
+//         console.log(`Hola soy el pokemon ${poke.name}`);
+//         // return obtenerPokemon()
+//     })
+//     .catch(onError)
