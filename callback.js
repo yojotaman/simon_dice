6d59fb27c94bb84e5a914ca4cd75f116eb950b5a
jonathan <josagal.ing@gmail.com>
2020-04-18 16:49:07 -0500

    se reescribe la funcion obtenerPokemon para que retorne una promesa

--- callback.js
+++ callback.js
@@ -2,33 +2,39 @@ const API_URL = 'https://pokeapi.co/api/v2/'
 const POKE_URL = 'pokemon/:id/'
 const opts = { crossDomain: true }
 
-function obtenerPokemon(id, callback) {
-    const url = `${API_URL}${POKE_URL.replace(':id',id)}`
-    $
-        .get(url, opts, callback)
-        .fail(function() {
-            console.log(`Ha ocurrido un error de conexión, no fue posible obtener el pokemon con id: ${id}`);
-        })
+// function obtenerPokemon(id, callback) {
+
+//     const url = `${API_URL}${POKE_URL.replace(':id',id)}`
+//     $
+//         .get(url, opts, callback)
+//         .fail(function() {
+//             console.log(`Ha ocurrido un error de conexión, no fue posible obtener el pokemon con id: ${id}`);
+//         })
+// }
+
+// obtenerPokemon(1, function(pokemon) {
+//     console.log(`Hola, mi nombre es ${pokemon.name}`);
+// })
+
+
+function obtenerPokemon(id) {
+    return new Promise((resolve, reject) => {
+        const url = `${API_URL}${POKE_URL.replace(':id',id)}`
+        $
+            .get(url, opts, function(data) {
+                resolve(data)
+            })
+            .fail(() => reject(id))
+    })
+
 }
 
-obtenerPokemon(1, function(pokemon) {
-    console.log(`Hola, mi nombre es ${pokemon.name}`);
+function onError(id) {
+    console.log(`Error obteniendo el id ${id}`);
+}
 
-    obtenerPokemon(2, function(pokemon) {
-
-        console.log(`Hola, mi nombre es ${pokemon.name}`);
-        debugger
-
-        obtenerPokemon(3, function(pokemon) {
-            console.log(`Hola, mi nombre es ${pokemon.name}`);
-
-            obtenerPokemon(4, function(pokemon) {
-                console.log(`Hola, mi nombre es ${pokemon.name}`);
-
-                obtenerPokemon(5, function(pokemon) {
-                    console.log(`Hola, mi nombre es ${pokemon.name}`);
-                })
-            })
-        })
+obtenerPokemon(1)
+    .then(function(poke) {
+        console.log(`Hola soy el pokemon  ${poke.name}`);
     })
-})
+    .catch(onError)
