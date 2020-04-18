2152c7e338f524dd0c750b6c64533dab5d8f07aa
jonathan <josagal.ing@gmail.com>
2020-04-18 15:47:29 -0500

    se realiza ajuste para manejo apropiado de callback y se agrega manjador deerror de conexion a la red

--- callback.js
+++ callback.js
@@ -4,19 +4,30 @@ const opts = { crossDomain: true }
 
 function obtenerPokemon(id, callback) {
     const url = `${API_URL}${POKE_URL.replace(':id',id)}`
-    $.get(url, opts, function(pokemon) {
-        console.log(`Hola, mi nombre es ${pokemon.name}`);
-    })
-    if (callback) {
-        callback()
-    }
+    $
+        .get(url, opts, callback)
+        .fail(function() {
+            console.log(`Ha ocurrido un error de conexión, no fue posible obtener el pokemon con id: ${id}`);
+        })
 }
 
-obtenerPokemon(1, function() {
-    obtenerPokemon(2, function() {
-        obtenerPokemon(3, function() {
-            obtenerPokemon(4, function() {
-                obtenerPokemon(5)
+obtenerPokemon(1, function(pokemon) {
+    console.log(`Hola, mi nombre es ${pokemon.name}`);
+
+    obtenerPokemon(2, function(pokemon) {
+
+        console.log(`Hola, mi nombre es ${pokemon.name}`);
+        debugger
+
+        obtenerPokemon(3, function(pokemon) {
+            console.log(`Hola, mi nombre es ${pokemon.name}`);
+
+            obtenerPokemon(4, function(pokemon) {
+                console.log(`Hola, mi nombre es ${pokemon.name}`);
+
+                obtenerPokemon(5, function(pokemon) {
+                    console.log(`Hola, mi nombre es ${pokemon.name}`);
+                })
             })
         })
     })
